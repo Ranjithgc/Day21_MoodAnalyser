44d9fc42194c987b7a033e070b710cb3fdd491df
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-17 16:30:10 +0530

    Added MoodAnalyzer program

---
+++ main/java/com/bridgelabz/MoodAnalyzer/MoodAnalisysException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalisysException extends Exception {
+
+	public  MoodAnalisysException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyser.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalyser {
+
+	/**
+	 * 
+	 * @param string
+	 * @return String
+	 * This gives the mood 
+	 */
+	public String analyseMood(String string) throws MoodAnalisysException  {             
+		try {
+			if (string.contains("SAD")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
+			throw new MoodAnalisysException("please enter proper message");
+		} 
+	}
+
+}
